4e6839ecec5bc477de00ad864a81e254d6608991
Molly <mollykmcglone@gmail.com>
2016-07-18 15:52:59 -0700

    add employee page to front end

--- app.rb
+++ app.rb
@@ -23,55 +23,66 @@ post('/employees') do
 end
 
 post('/divisions') do
-  name = params.fetch("name")
+  name = params.fetch('name')
   @division = Division.create({:name => name})
   @divisions = Division.all()
+  @employees = Employee.all()
   erb(:index)
 end
 
 get("/divisions/:id") do
+  @division = Division.find(params.fetch('id').to_i())
   @divisions = Division.all()
   @employees = Employee.all()
-  @division = Division.find(params.fetch("id").to_i())
   erb(:division)
 end
 
 get("/divisions/:id/edit") do
-  @division = Division.find(params.fetch("id").to_i())
+  @division = Division.find(params.fetch('id').to_i())
   erb(:division_edit)
 end
 
 patch("/divisions/:id") do
   name = params.fetch("name")
-  @division = List.find(params.fetch("id").to_i())
+  @division = List.find(params.fetch('id').to_i())
   @division.update({:name => name})
-  erb(:division)
+  erb(:success)
 end
 
 delete("/divisions/:id") do
-  @division = Division.find(params.fetch("id").to_i())
+  @division = Division.find(params.fetch('id').to_i())
   @division.delete()
   @divisions = Division.all()
-  erb(:index)
+  erb(:success)
+end
+
+get("/employees/:id") do
+  @employee = Employee.find(params.fetch('id').to_i())
+  @divisions = Division.all()
+  @employees = Employee.all()
+  erb(:employee)
 end
 
 get("/employees/:id/edit") do
   @employee = Employee.find(params.fetch("id").to_i())
+  @divisions = Division.all()
   erb(:employee_edit)
 end
 
 patch("/employees/:id") do
   name = params.fetch("name")
-  @employee = Employee.find(params.fetch("id").to_i())
-  @employee.update({:name => name})
+  division_id = params.fetch("division_id").to_i()
+  @employee = Employee.find(params.fetch('id').to_i())
+  @employee.update({:name => name, :division_id => division_id})
   @employees = Employee.all()
   @divisions = Division.all()
-  erb(:index)
+  erb(:success)
 end
 
 delete("/employees/:id") do
-  @employee = Employee.find(params.fetch("id").to_i())
+  @employee = Employee.find(params.fetch('id').to_i())
   @employee.delete()
   @employees = Employee.all()
-  erb(:index)
+  @divisions = Division.all()
+  erb(:success)
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,5 +1,11 @@
 require('spec_helper.rb')
 
 describe(Employee) do
-
+  describe('#division') do
+    it("tells which division it belongs to") do
+      test_division = Division.create({:name => "division"})
+      test_employee = Employee.create({:name => "employee", :division_id => test_division.id})
+      expect(test_employee.division()).to(eq(test_division))
+    end
+  end
 end
